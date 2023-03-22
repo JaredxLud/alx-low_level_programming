2e1a933c334ddfeb9f6ea7bca66c68efa60a60d7
Jared <jaredlud301@gmail.com>
2023-03-22 14:33:50 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,10 +11,10 @@ void print_alphabet_x10(void)
 
 	for (j = 1; i <= 10; i++)
 	{
-		for (j = 1; j <= 10; j++)
+		for (j = 97; j <= 122; j++)
 		{
 			_putchar(j);
-		}	
+		}
 		_putchar('\n');
 	}
 }
