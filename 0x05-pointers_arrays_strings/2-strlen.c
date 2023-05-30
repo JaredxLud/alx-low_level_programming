1c7d07b78c643fd3a521377a798a9d2901048b98
JaredxLud <jaredlud301@gmail.com>
2023-05-30 01:21:06 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * return: length
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while(*s != '\0')
+	{
+		longii++;
+		s++;
+	}
+	return (longi);
+}
+
