6433c38c270d898a0fd15f69055a5945880d5a0a
JaredxLud <jaredlud301@gmail.com>
2023-08-27 12:40:59 -0700

    0. A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-		
-int _putchar(char c);		
+int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
@@ -22,4 +21,3 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 #endif
-
