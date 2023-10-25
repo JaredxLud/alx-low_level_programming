1c9dd4b8406fde0d60002b1397f6da59f97cf94d
Jaredxlud <jaredlud301@gmail.com>
2023-10-25 14:37:48 -0700

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,3 @@
-#include "lists.h"
 #include <stdio.h>
 
 /**
@@ -6,18 +5,44 @@
  * @h: linked list of type listint_t to print
  *
  * Return: number of nodes
+ *
  */
+// Define the structure for a node in the list
+struct listint_t {
+	int data;
+	struct listint_t *next;
+};
 
-size_t print_listint(const listint_t *h)
-{
-	size_t num = 0;
 
-	while (h)
-	{
-		nodes++;
-		printf("%d\n", h->n);
+// Function to print all elements of a list
+size_t print_listint(const struct listint_t *h) {
+	size_t count = 0; // Initialize a count variable
+
+	while (h != NULL) {
+		printf("%d\n", h->data);
 		h = h->next;
+		count++;
 	}
 
-	return (nodes);
+
+	return count;
+}
+
+int main() {
+	// Example usage
+	struct listint_t node1, node2, node3;
+	node1.data = 1;
+	node2.data = 2;
+	node3.data = 3;
+
+	node1.next = &node2;
+	node2.next = &node3;
+	node3.next =NULL;
+
+	// Call the function to print the list and get the count
+	size_t count = print_listint(&node1);
+
+	printf("Count: %lu\n", count);
+
+	return 0;
 }
